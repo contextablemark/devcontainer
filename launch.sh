caa2c3e5912c8ae98c66e84103ab408877d42017
Mark Fogle <mark@contextable.com>
2026-01-10 21:13:53 -0800

    Added ability to name container.

--- launch.sh
+++ launch.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
-CONTAINER_NAME="dev-environment"
 IMAGE_NAME="dev-environment"
+DEFAULT_NAME="dev-environment"
+
+# Get container name from argument or prompt
+if [ -n "$1" ]; then
+    CONTAINER_NAME="$1"
+else
+    read -p "Container name [$DEFAULT_NAME]: " CONTAINER_NAME
+    CONTAINER_NAME="${CONTAINER_NAME:-$DEFAULT_NAME}"
+fi
 
 # Check if container exists
 if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
@@ -18,7 +26,7 @@ else
     echo "Creating and starting container '$CONTAINER_NAME'..."
     docker run -d \
         --name "$CONTAINER_NAME" \
-        --hostname dev \
+        --hostname "$CONTAINER_NAME" \
         -v "$HOME/.ssh:/home/developer/.ssh:ro" \
         -v "$HOME/.vscode-server:/home/developer/.vscode-server" \
         "$IMAGE_NAME" \
